28f0b80825c10646c0b4f802675fb5bb411cbb46
i.krikunov <i.krikunov@gitlab.tcsbank.ru>
2024-10-01 19:36:49 +0300

    feat(sprint_2_task_3): вывод кол-ва реплик

--- mongo-sharding-repl/scripts/add_data.sh
+++
@@ -1,8 +0,0 @@
-###
-# Инициализация бд
-###
-
-docker compose exec -T mongos_router1 mongosh --port 27020 --quiet <<EOF
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
-EOF
