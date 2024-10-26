8332e6c54f79b42e8d42020863c2bcfb366f08bd
Aleksandr Zagaevskiy <aleksandr.zagaevskiy@axxonsoft.dev>
2024-10-26 23:37:22 +0300

    add: mongo-sharding variant

---
+++ mongo-sharding/scripts/insert-data.sh
@@ -0,0 +1,9 @@
+###
+# Инициализируем БД данными
+###
+
+docker compose exec -T mongos_router mongosh <<EOF
+use somedb
+for(var i = 0; i < 1000; i++)
+  db.helloDoc.insertOne({age:i, name:"ly"+i})
+EOF
